60f536221d3c0698926a995b4fdfbdaa8235ee50
Upendra Rai <upendrarai02@gmail.com>
2024-12-09 10:01:36 +0530

    add more methods reverse string.

--- src/reverse/string/ReverseString.java
+++ src/reverse/string/ReverseString.java
@@ -1,12 +1,22 @@
 package reverse.string;
 
 public class ReverseString {
+
 	public static void main(String[] args) {
+
 		String str = "UPENDRARAI";
+
 		ReverseStringMethod1(str);
+
 		ReverseStringMethod2(str);
+
 		ReverseStringMethod3(str);
+
 		System.out.println("Using Recursion = " + ReverseStringMethod4(str));
+
+		System.out.println("Single loop = " + ReverseStringMethod5(str));
+
+		ReverseStringMethod6("Tail recursion for better optimization = " + ReverseStringMethod5(str));
 	}
 
 	// Here I am Using String simple for loop to Reverse String
@@ -15,6 +25,7 @@ public class ReverseString {
 		for (int i = str.length() - 1; i >= 0; i--) {
 			reversed.append(str.charAt(i));
 		}
+
 		System.out.println("ReverseStringMethod1 = " + reversed.toString());
 		// ReverseStringMethod1 = IARARDNEPU
 	}
@@ -24,6 +35,7 @@ public class ReverseString {
 		StringBuilder reversed = new StringBuilder(str);
 		System.out.println("ReverseStringMethod2 = " + reversed.reverse().toString());
 		// ReverseStringMethod2 = IARARDNEPU
+
 	}
 
 	// Here I am Convert String to charArray to Reverse String
@@ -36,12 +48,38 @@ public class ReverseString {
 		System.out.println();
 	}
 
-	// Here I am Using Recursion Method to Reverse String
+// Here I am Using Recursion Method to Reverse String
 	private static String ReverseStringMethod4(String str) {
 		if (str.isEmpty()) {
 			return str;
 		}
 		return str.charAt(str.length() - 1) + ReverseStringMethod4(str.substring(0, str.length() - 1));
+	}
+
+// Using charArray and a single loop to reverse a string
+	private static String ReverseStringMethod5(String str) {
+		char[] charArray = str.toCharArray();
+		int left = 0, right = charArray.length - 1;
+		while (left < right) {
+			char temp = charArray[left];
+			charArray[left] = charArray[right];
+			charArray[right] = temp;
+			left++;
+			right--;
+		}
+		return new String(charArray);
+	}
+
+// Using recursion to reverse a string (tail recursion for better optimization)
+	private static String ReverseStringMethod6(String str) {
+		return reverseRecursively(str, str.length() - 1);
+	}
+
+	private static String reverseRecursively(String str, int index) {
+		if (index < 0) {
+			return "";
+		}
+		return str.charAt(index) + reverseRecursively(str, index - 1);
 
 	}
 
